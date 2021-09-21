5d3d53da7a44505c1359e9182c9153fb8a8e0b16
Pranjal Lotankar <85273165+Pranjal-Lotankar@users.noreply.github.com>
2021-09-21 07:58:15 +0530

    Update prac2a.py

--- practical2/prac2a.py
+++ practical2/prac2a.py
@@ -1,3 +1,4 @@
+#Write a function that takes a character (i.e. a string of length 1) and returns True if it is a vowel, False otherwise.
 def check(c):
     if(c=="a" or c=="A" or c=="e" or c=="E" or c=="i" or c=="I" or c=="o" or c=="O" or c=="u" or c=="U"):
         return "True"
