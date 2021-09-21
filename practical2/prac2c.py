3dee103dd15467eae25ad9640d4e4f1045d54319
Pranjal Lotankar <85273165+Pranjal-Lotankar@users.noreply.github.com>
2021-09-21 07:56:58 +0530

    Update prac2c.py

--- practical2/prac2c.py
+++ practical2/prac2c.py
@@ -1,3 +1,9 @@
+'''Define a procedurehistogram() that takes a list of integers and prints a 
+histogram to the screen. For example, histogram([4, 9, 7]) should print the 
+following:
+****
+*********
+*******'''
 def histogram(l1):
     for i in l1:
         print('*' * i)
