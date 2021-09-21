d0341c597157d878ce7ef25608886c0046e51cc2
Pranjal Lotankar <85273165+Pranjal-Lotankar@users.noreply.github.com>
2021-09-21 11:16:17 +0530

    Update prac1b.py

--- practical1/prac1b.py
+++ practical1/prac1b.py
@@ -1,8 +1,4 @@
-"""
-Python program to check if the input number is odd or even.
-A number is even if division by 2 give a reminder of 0.
-If reminder is 1, it is odd number.
-"""
+#Python program to check if the input number is odd or even. A number is even if division by 2 give a reminder of 0. If reminder is 1, it is odd number.
 num= int(input("Enter a number:"))
 if (num%2)==0:
     print("{} is Even",format(num))
