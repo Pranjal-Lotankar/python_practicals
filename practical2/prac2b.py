5a47c6e6cbde1977c0f8387d59918db80ee81300
Pranjal Lotankar <85273165+Pranjal-Lotankar@users.noreply.github.com>
2021-09-22 20:39:20 +0530

    Update prac2b.py

--- practical2/prac2b.py
+++ practical2/prac2b.py
@@ -1,4 +1,5 @@
 #Define a function that computes the length of a given list or string.
+
 def len(s):
 	count = 0
 	for i in s:
@@ -7,4 +8,5 @@ def len(s):
 	return count
 
 print('The total length of the string is: ', count)
-
+ustr=input()
+len(ustr)
